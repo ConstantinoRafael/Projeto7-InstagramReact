fdebcf758e816e3776a50ac384c75cbcfaf1a4eb
ConstantinoRafael <rafael.constantino89@gmail.com>
2022-09-19 00:41:08 -0300

    feat: Mudando a foto e o nome

--- src/Sugestoes.js
+++ src/Sugestoes.js
@@ -1,4 +1,28 @@
+function Sugestao(props) {
+    return (
+        <div class="sugestao">
+            <div class="usuario">
+                <img src={props.imagem} />
+                <div class="texto">
+                    <div class="nome">{props.nome}</div>
+                    <div class="razao">{props.razao}</div>
+                </div>
+            </div>
+
+            <div class="seguir">Seguir</div>
+        </div>
+    )
+}
+
 export default function Sugestoes() {
+    const sugestao = [
+        { imagem: "assets/img/bad.vibes.memes.svg", nome: "bad.vibes.memes", razao: "Segue você"},
+        { imagem: "assets/img/chibirdart.svg", nome: "chibirdart", razao: "Segue você"},
+        { imagem: "assets/img/razoesparaacreditar.svg", nome: "razoesparaacreditar", razao: "Novo no Instagram"},
+        { imagem: "assets/img/adorable_animals.svg", nome: "adorable_animals", razao: "Segue você"},
+        { imagem: "assets/img/smallcutecats.svg", nome: "smallcutecats", razao: "Segue você"}
+    ]
+
     return (
         <div class="sugestoes">
             <div class="titulo">
@@ -6,65 +30,7 @@ export default function Sugestoes() {
                 <div>Ver tudo</div>
             </div>
 
-            <div class="sugestao">
-                <div class="usuario">
-                    <img src="assets/img/bad.vibes.memes.svg" />
-                    <div class="texto">
-                        <div class="nome">bad.vibes.memes</div>
-                        <div class="razao">Segue você</div>
-                    </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                    <img src="assets/img/chibirdart.svg" />
-                    <div class="texto">
-                        <div class="nome">chibirdart</div>
-                        <div class="razao">Segue você</div>
-                    </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                    <img src="assets/img/razoesparaacreditar.svg" />
-                    <div class="texto">
-                        <div class="nome">razoesparaacreditar</div>
-                        <div class="razao">Novo no Instagram</div>
-                    </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                    <img src="assets/img/adorable_animals.svg" />
-                    <div class="texto">
-                        <div class="nome">adorable_animals</div>
-                        <div class="razao">Segue você</div>
-                    </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                    <img src="assets/img/smallcutecats.svg" />
-                    <div class="texto">
-                        <div class="nome">smallcutecats</div>
-                        <div class="razao">Segue você</div>
-                    </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
+            {sugestao.map((s) => (<Sugestao imagem = {s.imagem} nome = {s.nome} razao = {s.razao} />))}
         </div>
     )
 }
--- src/Usuario.js
+++ src/Usuario.js
@@ -1,12 +1,28 @@
+import React from "react";
+
+
 export default function Usuario() {
+    const [nome, setNome] = React.useState("Catana")
+    const [imagem, setImagem] = React.useState("assets/img/catanacomics.svg")
+
+    function trocarNome() {
+        const nomePrompt = prompt("Qual o seu nome?");
+        setNome(nomePrompt);
+    }
+
+    function trocaImagem() {
+        const imagemPrompt = prompt("Qual a imagem nova?");
+        setImagem(imagemPrompt);
+    }
+
     return (
         <div class="usuario">
-            <img src="assets/img/catanacomics.svg" />
+            <img src={imagem} onClick={trocaImagem}/>
             <div class="texto">
                 <strong>catanacomics</strong>
                 <span>
-                    Catana
-                    <ion-icon name="pencil"></ion-icon>
+                    {nome}
+                    <ion-icon name="pencil" onClick={trocarNome}></ion-icon>
                 </span>
             </div>
         </div>
